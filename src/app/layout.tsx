0a6aa8aae299dc1800f73c6f10c0f771d9b9d9a4
June <105405983+junetheardist@users.noreply.github.com>
2024-05-09 12:26:30 +0100

    Last minutes touches to the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} >
         <main>
-          <Header />
+        
           {children}
           <Footer />
         </main>
--- src/app/resources/layout.tsx
+++ src/app/resources/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+
 import Footer from "@/components/Footer";
 import DocHeader from "@/components/DocHeader";
 import { Inter } from "next/font/google";
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 
 
-export default function Layout({
+export default function Layout ({
   children,
 }: Readonly<{
   children: React.ReactNode;
